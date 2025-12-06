f424eda6d98e9a3d603131e67beeb569a1b0e24d
NPY <yigehd@foxmail.com>
2025-12-06 22:23:01 +0800

    增加actions

---
+++ docker.sh
@@ -0,0 +1,11 @@
+docker stop ztools-container
+docker rm ztools-container
+docker rmi ztools-container
+docker build -t ztools-container .
+
+docker run -d \
+-p 80:80 \
+--name ztools-container \
+--restart on-failure:5 \
+-v /home/home/nginx.conf:/etc/nginx/conf.d/default.conf \
+ztools-container
